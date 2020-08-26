c472ac8c7f6b9da659d6dc2f9d494469f55c17b1
Yury Ignatev <yury.ignatev@jetbrains.com>
2020-08-26 16:07:42 +0700

    active color and truncate length arguments

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Tools for i3wm
 cmd/scratches
 --
 This tool allows you to display your scratchpad windows in polybar. When you click their names in bar, you toggle them.
+When scratchpad window is located on currently visible workspace its name is highlighted.
+
+*scratches* truncates window title up to 20 characters by default. If you need to disable it just pass `-trunacte 0` argument.
+If you have music player in scratchpad you can turn truncate off and see full now playing title, for example.
 
 ![](./img/scratches.png)
 
@@ -16,4 +20,18 @@ label = %output:0:20:...%
 format =  <label>
 format-foreground = ${colors.foreground-alt}
 click-left = scratches -s SCRATCHPAD_NAME -op toggle
+```
+
+**Command line arguments**
+
+```
+Usage of scratches:
+  -highlight string
+    	RGB color to highlight visible scratchpad window name (default "fff")
+  -op string
+    	operation on scratchpad: detect - to check if it exists, toggle - to toggle scratchpad (default "detect")
+  -s string
+    	scratchpad name
+  -truncate int
+    	truncate window title. 0 - to disable. (default 20)
 ```
--- cmd/scratches/main.go
+++ cmd/scratches/main.go
@@ -18,9 +18,13 @@ func main() {
 	var (
 		op             string
 		scratchPadName string
+		activeColor    string
+		truncate       int
 	)
 	flag.StringVar(&op, "op", OpDetect, "operation on scratchpad: detect - to check if it exists, toggle - to toggle scratchpad")
 	flag.StringVar(&scratchPadName, "s", "", "scratchpad name")
+	flag.StringVar(&activeColor, "highlight", "fff", "RGB color to highlight visible scratchpad window name")
+	flag.IntVar(&truncate, "truncate", 20, "truncate window title. 0 - to disable.")
 	flag.Parse()
 
 	if scratchPadName == "" {
@@ -47,9 +51,43 @@ func main() {
 		return
 	}
 
+	// search for foundNode workspace in currently open workspaces
+	visible := false
+	foundWorkspaces := i3tools.FindAll(tree.Root, func(node *i3.Node) bool {
+		if node.Type != i3.WorkspaceNode {
+			return false
+		}
+
+		if len(i3tools.FindAll(node, func(n *i3.Node) bool { return n.ID == foundNode.ID })) > 0 {
+			return true
+		}
+
+		return false
+	})
+	var foundWorkspace *i3.Node
+	if len(foundWorkspaces) > 0 {
+		foundWorkspace = foundWorkspaces[0]
+	}
+	if foundWorkspace != nil {
+		// get list of outputs and check if found workspace is visible now
+		outputs, err := i3.GetOutputs()
+		handleError(err)
+		for _, output := range outputs {
+			if output.CurrentWorkspace == foundWorkspace.Name {
+				visible = true
+				break
+			}
+		}
+	}
+
 	switch op {
 	case OpDetect:
-		fmt.Println(foundNode.Name)
+		truncated := truncateString(foundNode.Name, truncate)
+		if visible {
+			fmt.Printf("%%{F#%s}%s%%{F-}\n", activeColor, truncated)
+		} else {
+			fmt.Println(truncated)
+		}
 	case OpToggle:
 		_, _ = i3.RunCommand(fmt.Sprintf("[con_id=\"%d\"] scratchpad show", foundNode.ID))
 	}
@@ -63,6 +101,17 @@ func itemInStrings(list []string, item string) bool {
 		}
 	}
 	return false
+}
+
+func truncateString(s string, max int) string {
+	if max == 0 {
+		return s
+	}
+	runes := []rune(s)
+	if len(runes) < max {
+		return s
+	}
+	return string(runes[:max-1])
 }
 
 func handleError(err error) {
